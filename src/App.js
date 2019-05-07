cfe9e15b16e495425ef8304b67a01e317a17d33b
Lucas Pompa <lucas1.pompa@supinternet.fr>
2019-05-07 14:27:55 +0200

    push

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Home from './Components/Home'
+import Blog from './Components/Blog'
+import Header from './Components/Header'
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  render() {
+    return (
+      <Router>
+        <Route path="/home" component={Home} />
+        <Route path="/blog" component={Blog} />
+        <Route path="/header" component={Header} />
+      </Router>
+      
+    );
+  }
 }
 
 export default App;
---
+++ src/Components/Home.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+
+export default class Home extends Component {
+    render() {
+        return (
+        <div>
+                page d'acceuil
+        </div>
+        )
+    }
+}
